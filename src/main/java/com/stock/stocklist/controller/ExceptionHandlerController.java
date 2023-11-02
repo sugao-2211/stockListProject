fa9e6ff20f60861c99ecccf261e381a226777ab7
sugao-2211 <mat.17593.002@gmail.com>
2023-11-02 17:29:28 +0900

    ExceptionHandlerを別クラスに移動

---
+++ src/main/java/com/stock/stocklist/controller/ExceptionHandlerController.java
@@ -0,0 +1,27 @@
+package com.stock.stocklist.controller;
+
+import com.stock.stocklist.exception.NotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.ZonedDateTime;
+import java.util.Map;
+
+@RestControllerAdvice
+public class ExceptionHandlerController {
+
+    @ExceptionHandler(value = NotFoundException.class)
+    public ResponseEntity<Map<String, String>> handleNoResourceFound(
+            NotFoundException e, HttpServletRequest request) {
+        Map<String, String> body = Map.of(
+                "timestamp", ZonedDateTime.now().toString(),
+                "status", String.valueOf(HttpStatus.NOT_FOUND.value()),
+                "error", HttpStatus.NOT_FOUND.getReasonPhrase(),
+                "message", e.getMessage(),
+                "path", request.getRequestURI());
+        return new ResponseEntity(body, HttpStatus.NOT_FOUND);
+    }
+}
