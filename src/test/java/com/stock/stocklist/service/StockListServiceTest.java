3b01b465d164d0a96c14e38e77bc365ee0c6b4ed
sugao-2211 <mat.17593.002@gmail.com>
2023-11-28 23:23:36 +0900

    テストコードの実装

--- src/test/java/com/stock/stocklist/service/StockListServiceTest.java
+++ src/test/java/com/stock/stocklist/service/StockListServiceTest.java
@@ -1,5 +1,35 @@
 package com.stock.stocklist.service;
 
+import com.stock.stocklist.entity.StockList;
+import com.stock.stocklist.mapper.StockListMapper;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDate;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.doReturn;
+
+@ExtendWith(MockitoExtension.class)
 class StockListServiceTest {
+
+    @InjectMocks
+    StockListService stockListService;
+
+    @Mock
+    StockListMapper stockListMapper;
+    
+    @Test
+    public void 存在する在庫のIDを指定したときに正常に在庫の情報が返されること() throws Exception {
+        doReturn(Optional.of(new StockList(1, "メタノール", "HPLC用", 3, "L", LocalDate.parse("2023-05-24"))))
+                .when(stockListMapper).findById(1);
+
+        StockList actual = stockListService.findById(1);
+        assertThat(actual).isEqualTo(new StockList(1, "メタノール", "HPLC用", 3, "L", LocalDate.parse("2023-05-24")));
+    }
 
 }
