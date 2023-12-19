8cde3ae36365da4f3f03d5b74c6223415cc2a4e4
sugao-2211 <mat.17593.002@gmail.com>
2023-12-19 21:39:49 +0900

    テストコードを模写して実装

--- src/test/java/integrationtest/StockListApiIntegrationTest.java
+++ src/test/java/integrationtest/StockListApiIntegrationTest.java
@@ -1,18 +1,53 @@
 package integrationtest;
 
+import com.github.database.rider.core.api.dataset.DataSet;
+import com.github.database.rider.spring.api.DBRider;
+import org.junit.jupiter.api.Test;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.skyscreamer.jsonassert.JSONCompareMode;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.nio.charset.StandardCharsets;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@DBRider
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class StockListApiIntegrationTest {
 
     @Autowired
     MockMvc mockMvc;
 
-    void 在庫情報が全件取得できること() {
+    @Test
+    @DataSet(value = "datasets/stockList.yml")
+    @Transactional
+    void 在庫情報が全件取得できること() throws Exception {
+        String response = mockMvc.perform(MockMvcRequestBuilders.get("/api/users"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn().getResponse().getContentAsString(StandardCharsets.UTF_8);
+
+        JSONAssert.assertEquals("[" +
+                " {" +
+                " \"id\": 1," +
+                " \"name\": \"清⽔\"" +
+                " }," +
+                " {" +
+                " \"id\": 2," +
+                " \"name\": \"⼩⼭\"" +
+                " }," +
+                " {" +
+                " \"id\": 3," +
+                " \"name\": \"⽥中\"" +
+                " }" +
+                "]", response, JSONCompareMode.STRICT);
+
     }
 
 }
