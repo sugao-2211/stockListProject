fa79b458aaf877cd1a647cc9202e7fbad63b7097
sugao-2211 <mat.17593.002@gmail.com>
2023-12-19 21:24:58 +0900

    StockListApiIntegrationTestの作成

---
+++ src/test/java/integrationtest/StockListApiIntegrationTest.java
@@ -0,0 +1,18 @@
+package integrationtest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class StockListApiIntegrationTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    void 在庫情報が全件取得できること() {
+    }
+
+}
